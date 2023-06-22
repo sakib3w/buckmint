5862b42b70250ac85a9c597bdb304e1ccfd4704c
Abdullah-Sakib <abdullahsakib888@gmail.com>
2023-06-22 14:02:23 +0600

    delete: styles cleaning done except components

--- src/env.js
+++ src/env.js
@@ -1,6 +1,5 @@
 export const env = {
   SECRET_KEY:
-    "71e1518c1cc9c5ffca9f68bb813b8fbf9d90008be91da2fc2f830d42b61633d887ec05cadbdbc9ec9ee168cf4b7b3846c7619f027db5c5b1339e2105826ba465",
+    "71e1518c1cc9c5ffca9f68bb813b8fbf9d92008be91da2fc2f830d42b61633d287ec05cadbdbc9ec9ee168cf4b7b3846c7619f027db5c5b1339e2105826ba465",
   // BASE_URL: "http://localhost:1000",
-  BASE_URL: "https://rightfuture.triplewsols.in",
 };
